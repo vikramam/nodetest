37d04bea3766ece45c89ea11c63c0a8ba49cad87
vikramam <32703231+vikramam@users.noreply.github.com>
2023-10-19 22:05:46 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+   
+    var sql = require("mssql");
+
+    // config for your database
+    var config = {
+        user: 'db_aa0535_testdb_admin',
+        password: 'Mulgund@85',
+        server: 'SQL5108.site4now.net', // You can use 'localhost\\instance' to connect to named instance 
+        database: 'db_aa0535_testdb',   
+    };
+
+    // connect to your database
+    sql.connect(config, function (err) {
+    
+        if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request();
+           
+        // query to the database and get the records
+        request.query('select * from usertable', function (err, recordset) {
+            
+            if (err) console.log(err)
+
+            // send records as a response
+            res.send(recordset);
+            
+        });
+    });
+});
+
+var server = app.listen(5000, function () {
+    console.log('Server is running..');
+});
